9ae6e957e927790bb6f6ae856846eb74f3515fa1
EtienneViana <etienne.viana@gmail.com>
2020-01-28 00:40:10 +0100

    Project submitted and validated

---
+++ checker.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   checker.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eviana <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/03/20 16:27:23 by eviana            #+#    #+#             */
+/*   Updated: 2019/04/15 11:06:34 by eviana           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+static void	answer_print(t_pile *pile_a, t_pile *pile_b)
+{
+	if (pile_b->size == 0)
+	{
+		if (sp_is_increasing(pile_a->tab, pile_a->size) || pile_a->size == 1)
+			ft_putstr("OK\n");
+		else
+			ft_putstr("KO\n");
+	}
+	else
+		ft_putstr("KO\n");
+}
+
+int			main(int argc, char **argv)
+{
+	char	*line;
+	t_pile	*pile_a;
+	t_pile	*pile_b;
+	t_pile	*pile_perfect;
+
+	if (argc < 2 || !check_alldigit(argc, argv))
+		put_error(NULL, NULL, NULL, NULL);
+	if (!(pile_a = pile_get(argc, argv)))
+		put_error(NULL, NULL, NULL, NULL);
+	if (!(pile_b = pile_get(1, NULL)))
+		put_error(pile_a, NULL, NULL, NULL);
+	if (!(pile_perfect = pile_quick_sort(pile_a)))
+		put_error(pile_a, pile_b, NULL, NULL);
+	if (!sp_is_increasing(pile_perfect->tab, pile_perfect->size))
+		put_error(pile_a, pile_b, pile_perfect, NULL);
+	while (get_next_line(0, &line) == 1)
+	{
+		if (!action_apply(action_nbr(line), pile_a, pile_b))
+			put_error(pile_a, pile_b, pile_perfect, line);
+		ft_strdel(&line);
+	}
+	answer_print(pile_a, pile_b);
+	del_all(pile_a, pile_b, pile_perfect, NULL);
+	ft_strdel(&line);
+	return (0);
+}
